5290f5598eed7e046f5014a836255eaf58c9c9a6
DavidAkerlind <david.akerlind@icloud.com>
2025-04-15 10:10:09 +0200

    Done with MovieList, MovieItem and added a SearchFetch-Hook

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import './movieList.css';
+import MovieItem from '../MovieItem/MovieItem';
 
-function MovieList() {
-	return <div></div>;
+function MovieList({ movieList, title }) {
+	return (
+		<section className="list-section">
+			<h1 className="list-section__header">{title}</h1>
+			<ul className="list-section__grid">
+				{movieList.map((movie, index) => (
+					<MovieItem key={index} movie={movie} />
+				))}
+			</ul>
+		</section>
+	);
 }
 
 export default MovieList;
