a576287eebf402a1a2c8267fc43d7a3c53e5bc9c
Welene <helenetheo@live.no>
2025-04-15 18:04:28 +0200

    Imported Header component

--- src/pages/ResultsPage/ResultsPage.jsx
+++ src/pages/ResultsPage/ResultsPage.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import Header from '../../components/Header/Header';
 
 function ResultsPage() {
-	return <section className="page page-results"></section>;
+	return (
+		<section className="page page-results">
+			<Header />
+		</section>
+	);
 }
 
 export default ResultsPage;
