a2b24f303706253e710c25caaa715fa6c86401c1
Welene <helenetheo@live.no>
2025-04-15 12:15:10 +0200

    Added comments, and small fixes to navigation/search input function

--- src/components/SearchInput/SearchInput.jsx
+++ src/components/SearchInput/SearchInput.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import './SearchInput.css';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useSearchFetch } from '../../hooks/useSearchFetch';
 
-const handleSearch(navigate, searchValue) => {
-	navigate (`/results/${searchValue}`);
+// function that navigates you to /results page + takes in navigate and searchValue prop = navigates to correct page BASED ON what searchValue is
+const handleSearch = (navigate, searchValue) => {
+	navigate(`/results/${searchValue}`);
+};
 
 function SearchInput() {
 	const [searchValue, setSearchValue] = useState('');
@@ -14,11 +15,22 @@ function SearchInput() {
 	// en hook fra react-biblioteket - lar deg bruke en funksjon (navigate-funksjonen) til å navigere når den sendes som prop til handleSearch
 	return (
 		<section className="search-section">
-			<input type="text" className="search-section__search-field" value = {searchValue} onChange={(e) => setSearchValue (e.target.value)} />
-			<button className="search-section__btn" onClick={() => handleSearch (navigate, searchValue)}>Search</button>
+			<input
+				type="text"
+				className="search-section__search-field"
+				value={searchValue}
+				// connects input 'value' with my searchValue, where the input value will be stored
+				onChange={(e) => setSearchValue(e.target.value)}
+				// input field listenes to a change (writing = change), takes that change and updates my setSearchValue to hold that user input
+			/>
+			<button
+				className="search-section__btn"
+				onClick={() => handleSearch(navigate, searchValue)}>
+				{/* button listens to a click - when clicked - calls handleSearch function and sends 'navigate' & 'searchValue' prop with it */}
+				Search
+			</button>
 		</section>
 	);
-	}
 }
 
 export default SearchInput;
