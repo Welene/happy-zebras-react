f4dbd87ac643ee80a62d68cf78a9d037d035b04e
Welene <helenetheo@live.no>
2025-04-16 14:03:58 +0200

    Made ResultsPage function // fetches API with user input // added header & movielist to page

--- src/pages/ResultsPage/ResultsPage.jsx
+++ src/pages/ResultsPage/ResultsPage.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import Header from '../../components/Header/Header';
+import { useParams } from 'react-router-dom';
+import { useSearchFetch } from '../../hooks/useSearchFetch';
+import MovieList from '../../components/MovieList/MovieList';
 
 function ResultsPage() {
+	const { searchValue } = useParams();
+	// henter searchValue fra URL ved hjelp av useParams funksjonen
+	const searchUrl = `http://www.omdbapi.com/?apikey=144f166d&s=${searchValue}`;
+	// tar API-URL-en fra useFetchSearch og legger på searchValue
+	const { data, isLoading, isError } = useSearchFetch(searchUrl);
+	// anropet useSearchFetch funksjonen og sender den searchUrl som parameter, sånn at funksjonen vet hva den skal søke på
 	return (
 		<section className="page page-results">
 			<Header />
+			<MovieList
+				title={'🎬 Search Results 🎬'}
+				movieList={Array.isArray(data) ? data : []}
+			/>
 		</section>
 	);
 }
