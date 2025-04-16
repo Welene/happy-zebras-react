bedd436f96695974864729345989e5decbdac556
Thapa-Pro <bachchan.k.t@gmail.com>
2025-04-16 10:04:00 +0200

    Edited HomePage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,18 +1,19 @@
+
 import './homePage.css';
 import Header from '../../components/Header/Header';
+import MovieList from '../../components/MovieList/MovieList';
 import Carousel from '../../components/Carousel/Carousel';
-import MovieItem from '../../components/MovieItem/MovieItem';
 
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import { useState, useEffect } from 'react';
 
 const HomePage = () => {
 	const [recommendedMovies, setRecommendedMovies] = useState([]);
-	const [favorites, setFavorites] = useState([]);
 	const [loading, setLoading] = useState(true);
 	const [error, setError] = useState(null);
 
 	useEffect(() => {
-		const fetchRecommendedMovies = async () => {
+		const fectchRecommendedMovies = async () => {
 			try {
 				const res = await fetch(
 					'https://santosnr6.github.io/Data/favoritemovies.json'
@@ -27,28 +28,14 @@ const HomePage = () => {
 			}
 		};
 
-		const savedFavorites = JSON.parse(localStorage.getItem('favorites')) || [];
-		setFavorites(savedFavorites);
-
-		fetchRecommendedMovies();
+		fectchRecommendedMovies();
 	}, []);
-
-	const toggleFavorite = (imdbID) => {
-		let updatedFavorites;
-		if (favorites.includes(imdbID)) {
-			updatedFavorites = favorites.filter(id => id !== imdbID);
-		} else {
-			updatedFavorites = [...favorites, imdbID];
-		}
-		setFavorites(updatedFavorites);
-		localStorage.setItem('favorites', JSON.stringify(updatedFavorites));
-	};
 
 	return (
 		<div className="page page-home">
 			<Header />
-
 			<main>
+				{/* Trailor Carousel from Helene */}
 				<section className="carousel-section">
 					<Carousel />
 				</section>
@@ -56,26 +43,11 @@ const HomePage = () => {
 				<section className="recommendation-section">
 					{loading && <p>Loading...</p>}
 					{error && <p className="error">{error}</p>}
-
 					{!loading && !error && (
-						<section className="list-section">
-							<h1 className="list-section__header">🎬 Our Recommendations 🎬</h1>
-
-							<ul className="list-section__grid">
-								{recommendedMovies.map((movie, index) => (
-									<li className="movie-card-wrapper" key={index}>
-										<div className="favorite-card-container">
-											<button
-												className={`favorite-btn ${favorites.includes(movie.imdbID) ? 'favorite' : ''
-													}`}
-												onClick={() => toggleFavorite(movie.imdbID)}
-											></button>
-											<MovieItem movie={movie} />
-										</div>
-									</li>
-								))}
-							</ul>
-						</section>
+						<MovieList
+							movieList={recommendedMovies}
+							title={'🎬 Our Recommendations 🎬'}
+						/>
 					)}
 				</section>
 			</main>
